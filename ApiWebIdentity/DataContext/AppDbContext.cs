67e444771569793d29f77424d63f4fed8b66402a
Antônio Rocha <antoniojunior159@gmail.com>
2024-04-06 16:06:06 -0300

    Add Identity Server

--- ApiWebIdentity/DataContext/AppDbContext.cs
+++ ApiWebIdentity/DataContext/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using ApiWebIdentity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApiWebIdentity.DataContext
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext //Retirar o DbContext para o Identity
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):
             base(options)
@@ -12,20 +13,24 @@ namespace ApiWebIdentity.DataContext
 
         public DbSet<Hero> Heroes { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Hero>().HasData(
-                new Hero
-                { 
-                    Id = 1,
-                    CivilName = "Peter Parker",
-                    HeroName = "Spider-Man",
-                    Email = "peter@gmail.com",
-                    Age = 28,
-                    City = "New York City"
-                }
-                );
-        }
+        //Retirando quando subistituiu o DbContext pelo IdentityDbContext
+        #region Comment
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Hero>().HasData(
+        //        new Hero
+        //        { 
+        //            Id = 1,
+        //            CivilName = "Peter Parker",
+        //            HeroName = "Spider-Man",
+        //            Email = "peter@gmail.com",
+        //            Age = 28,
+        //            City = "New York City"
+        //        }
+        //        );
+        //}
+
+        #endregion
     }
 }
